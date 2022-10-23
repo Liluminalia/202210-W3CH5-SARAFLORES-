8ee4e66133a13793f200dd52504b119ab1e311e2
Liluminalia <peloazul4@gmail.com>
2022-10-23 17:37:17 +0200

    trying pokemon item

--- dist/js/components/pokemon.item.js
+++ dist/js/components/pokemon.item.js
@@ -1,1 +1,17 @@
-"use strict";
+import { pokemon } from '../services/storage.js';
+import { Component } from './component.js';
+export class PokemonItem extends Component {
+    constructor(selector) {
+        super();
+        this.selector = selector;
+        this.template = this.createTemplate();
+        this.renderAdd(this.selector, this.template);
+    }
+    createTemplate() {
+        return `
+      <div class="pokemon-container">
+      <p class="pokemon__name">${pokemon.name}</p>`;
+        this.template += `<img src="${pokemon.sprites.other.home.front_default}" alt="Imagen de ${pokemon.species.name}" width="300">
+      </></div>`;
+    }
+}
